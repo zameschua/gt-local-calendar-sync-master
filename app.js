1624ae3250f65ffd6aecc87f3b17534199f0369d
zameschua <zames.chua@gmail.com>
2018-04-25 03:06:11 +0800

    Link up with Google Calendar API

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 var express = require('express');
 var app = express();
+require('./src/utils/googleCalendar.js');
 
 app.get('/', function (req, res) {
+    res.send('Hello World!');
+});
+
+app.get('/calendar-events', function (req, res) {
+    // If user is not logged in, redirect to login page, then redirect back to /calendar-events
+    // else, return a list of calendar events
     res.send('Hello World!');
 });
 
